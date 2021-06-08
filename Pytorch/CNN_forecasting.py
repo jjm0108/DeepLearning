a18204ff6d3c95bd5e1ea0afb161132d90c18ee1
jjm0108 <jjm0108@nepes.co.kr>
2021-06-08 16:09:55 +0900

    forecasting

--- Pytorch/CNN_forecasting.py
+++ Pytorch/CNN_forecasting.py
@@ -16,41 +16,37 @@ import os,sys
 sys.path.insert(0, os.getcwd()) 
 from data_analysis_module import *
 
-#df = pd.read_csv('DeepLearning/Pytorch/PV_Elec_Gas3.csv').rename(columns={'date':'timestamp'})
-df = ImportFDCData("1")[:100]
+def data_analysis(df):
+    print('*'*50)
+    print('data : \n{}'.format(df)) 
+    print('*'*50)
+    # print('\ndata type : \n{}'.format(df.dtypes))
+    # print('*'*50)
+    print(f'data length: {len(df)}') 
+    print('*'*50)
 
-print('*'*50)
-print('data : \n{}'.format(df)) 
-print('*'*50)
-# print('\ndata type : \n{}'.format(df.dtypes))
-# print('*'*50)
-print(f'data length: {len(df)}') 
-print('*'*50)
+def train_valid_test_split(train_set_split_ratio, valid_set_split_ratio):
+    # split dataset to train/valid
+    test_set_split_ratio = 1 - train_set_split_ratio - valid_set_split_ratio
+    train_set_split = int(len(df)*train_set_split_ratio) if int(len(df)*train_set_split_ratio)%2!=0 else int(len(df)*train_set_split_ratio)+1
+    valid_set_split = int(len(df)*(train_set_split_ratio + valid_set_split_ratio))
+    test_set_split = int(len(df)*test_set_split_ratio)
 
-# split dataset to train/valid
-#pred_param = 'Gas/day'
-pred_param = 'Ch 1 Onboard Cryo Temp 1st Stage'
+    train_set = df[:train_set_split]
+    valid_set = df[train_set_split:valid_set_split]
+    test_set = df[-test_set_split:]
 
-#split dataset to train/valid
-train_set_split_ratio = 0.4
-valid_set_split_ratio = 0.2
-test_set_split_ratio = 1 - train_set_split_ratio - valid_set_split_ratio
-train_set_split = int(len(df)*train_set_split_ratio) if int(len(df)*train_set_split_ratio)%2!=0 else int(len(df)*train_set_split_ratio)+1
-valid_set_split = int(len(df)*(train_set_split_ratio + valid_set_split_ratio))
-test_set_split = int(len(df)*test_set_split_ratio)
+    print(f'length of train_set : {len(train_set)}')
+    print(f'length of valid_set : {len(valid_set)}')
 
-train_set = df[:train_set_split]
-valid_set = df[train_set_split:valid_set_split]
-test_set = df[-test_set_split:]
+    # show the proportion of each dataset
+    print('*'*50)
+    print('Proportion of train_set : {:.2f}%'.format(len(train_set)/len(df)))
+    print('Proportion of valid_set : {:.2f}%'.format(len(valid_set)/len(df)))
+    print('*'*50)
 
-print(f'length of train_set : {len(train_set)}')
-print(f'length of valid_set : {len(valid_set)}')
-
-# show the proportion of each dataset
-print('*'*50)
-print('Proportion of train_set : {:.2f}%'.format(len(train_set)/len(df)))
-print('Proportion of valid_set : {:.2f}%'.format(len(valid_set)/len(df)))
-print('*'*50)
+    
+    return train_set, valid_set, test_set
 
 # The split_sequence() function below will split a given univariate sequence into multiple samples 
 # where each sample has a specified number of time steps and the output is a single time step.
@@ -66,19 +62,6 @@ def split_sequence(sequence, n_steps):
         x.append(seq_x)
         y.append(seq_y)
     return array(x), array(y)
-
-n_steps = 3
-train_x,train_y = split_sequence(train_set[pred_param].values,n_steps)
-valid_x,valid_y = split_sequence(valid_set[pred_param].values,n_steps)
-
-# summarize the data
-print("train_x, train_y splitted  : ")
-for i in range(10):
-	print(train_x[i], train_y[i])
-print('*'*50)
-print(f'length of train_x, train_y : {len(train_x)} , {len(train_y)}')
-print(f'length of valid_x, valid_y : {len(valid_x)} , {len(valid_y)}')
-print('*'*50)
 
 class MyDataset(Dataset):
     def __init__(self,feature,target):
@@ -112,29 +95,6 @@ class CNN_ForecastNet(nn.Module):
         x = self.fc2(x)
         
         return x
-
-# 학습을 위한 장치 얻기
-# 가능한 경우 GPU와 같은 하드웨어 가속기에서 모델을 학습
-# torch.cuda 를 사용할 수 있는지 확인하고 그렇지 않으면 CPU를 계속 사용
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-# model, optimizer, criterion 설정
-model = CNN_ForecastNet().to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-5) # Adam
-criterion = nn.MSELoss() # Mean Squared Error 
-
-# reshape from [samples, timesteps] into [samples, timesteps, features]
-n_features = 1 # for univariate series, the number of features is one, for one variable.
-train = MyDataset(train_x.reshape(train_x.shape[0],train_x.shape[1],n_features),train_y)
-valid = MyDataset(valid_x.reshape(valid_x.shape[0],valid_x.shape[1],n_features),valid_y)
-
-train_loader = torch.utils.data.DataLoader(train,batch_size=1,shuffle=False)
-valid_loader = torch.utils.data.DataLoader(valid,batch_size=1,shuffle=False)
-
-torch.set_default_tensor_type(torch.DoubleTensor)
-
-train_losses = []
-valid_losses = []
 
 def Train():
     running_loss = .0
@@ -174,7 +134,63 @@ def Valid():
         valid_losses.append(valid_loss.detach().numpy())
         print(f'valid_loss {valid_loss}')
 
-epochs = 10
+
+# data importing
+df = ImportFDCData("1")[:50000]
+data_analysis(df)
+
+pred_param = 'Ch 1 Onboard Cryo Temp 1st Stage'
+
+train_set, valid_set, test_set = train_valid_test_split(train_set_split_ratio = 0.4, valid_set_split_ratio =0.2)
+
+plt.figure(figsize=(16,8), dpi=100)
+plt.title("data split")
+plt.plot(train_set['time'], train_set[pred_param], label="train_set")
+plt.plot(valid_set['time'], valid_set[pred_param], label="valid_set")
+plt.plot(test_set['time'], test_set[pred_param], label="test_set")
+plt.legend(loc="upper right")
+plt.show()
+plt.pause(1)
+plt.close()
+
+n_steps = 3
+train_x,train_y = split_sequence(train_set[pred_param].values,n_steps)
+valid_x,valid_y = split_sequence(valid_set[pred_param].values,n_steps)
+
+# summarize the data
+print("train_x, train_y splitted  : ")
+for i in range(10):
+	print(train_x[i], train_y[i])
+print('*'*50)
+print(f'length of train_x, train_y : {len(train_x)} , {len(train_y)}')
+print(f'length of valid_x, valid_y : {len(valid_x)} , {len(valid_y)}')
+print('*'*50)
+
+
+# 학습을 위한 장치 얻기
+# 가능한 경우 GPU와 같은 하드웨어 가속기에서 모델을 학습
+# torch.cuda 를 사용할 수 있는지 확인하고 그렇지 않으면 CPU를 계속 사용
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# model, optimizer, criterion 설정
+model = CNN_ForecastNet().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5) # Adam
+criterion = nn.MSELoss() # Mean Squared Error 
+
+# reshape from [samples, timesteps] into [samples, timesteps, features]
+n_features = 1 # for univariate series, the number of features is one, for one variable.
+train = MyDataset(train_x.reshape(train_x.shape[0],train_x.shape[1],n_features),train_y)
+valid = MyDataset(valid_x.reshape(valid_x.shape[0],valid_x.shape[1],n_features),valid_y)
+
+train_loader = torch.utils.data.DataLoader(train,batch_size=1,shuffle=False)
+valid_loader = torch.utils.data.DataLoader(valid,batch_size=1,shuffle=False)
+
+torch.set_default_tensor_type(torch.DoubleTensor)
+
+train_losses = []
+valid_losses = []
+
+epochs = 100
 for epoch in range(epochs):
     print('epochs {}/{}'.format(epoch+1,epochs))
     Train()
@@ -191,10 +207,16 @@ for epoch in range(epochs):
         plt.pause(3)
         plt.close()
 
+path = './model/'
+os.makedirs(path, exist_ok=True)
+filename = f'FDC data {pred_param} prediction model'
+filename = os.path.join(path, filename)
+torch.save(model, filename+".pt")
+
 
 plt.plot(train_losses,label='train_loss')
 plt.plot(valid_losses,label='valid_loss')
-plt.title('Final MSE Loss')
+plt.title(f'Final MSE Loss (epoch {epochs})')
 plt.xlim(0, epochs)
 plt.ylim(0, 1)
 plt.legend(loc='upper left')
@@ -202,7 +224,7 @@ plt.legend(loc='upper left')
 test_x , test_y = split_sequence(test_set[pred_param].values,n_steps)
 inputs = test_x.reshape(test_x.shape[0],test_x.shape[1],n_features)
 print('*'*50)
-print(f'length of train_x, train_y : {len(test_x)} , {len(test_y)}')
+print(f'length of test_x, test_y : {len(test_x)} , {len(test_y)}')
 print('*'*50)
 
 model.eval()
@@ -211,15 +233,23 @@ batch_size = 2
 iterations =  int(inputs.shape[0])
 
 for i in range(iterations):
-    preds = model(torch.tensor([inputs[i]]).float())
-    prediction.append(preds.detach().numpy())
+    preds = model(torch.tensor([inputs[i]]).float()).detach().item()
+    prediction.append(preds)
 
 plt.figure(figsize=(16,8), dpi=100)
 plt.title("result")
-print(prediction)
-print(test_y)
-print(len(prediction))
-print(len(test_y))
 plt.plot(prediction, label="predicted one")
 plt.plot(test_y, label="real one")
+plt.legend(loc="upper right")
 plt.show()
+
+# anomaly_score = np.array(prediction)-np.array(test_y)
+
+# fig, ax = plt.subplots(3, 1,figsize=(11,6))
+# ax[0].set_title("result")
+# ax[0].plot(test_y, label="real one")
+# ax[1].plot(prediction, label="predicted one")
+# ax[2].plot(anomaly_score, label="target-prediction")
+
+# plt.show()
+
